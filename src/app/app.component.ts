4ccaae378eba7f1edfcf66fc79c6853f732d76dd
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-20 10:58:54 -0300

    retirando função do app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {ServiceApiService} from './service-api.service'
-import { ModelResponse } from './buscar-clima/model.response';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Weather Project';
-
-  estado:string = "Salvador"
-  model:ModelResponse[];
-  modelCall
-  teste:string = "Teste";
-  constructor(private myservice: ServiceApiService){}
-
-  ngOnInit() {
-    this.myservice.getData(this.estado).subscribe((data) => {
-      let resultData = Object.create(data).data[0];
-      this.modelCall = new ModelResponse(resultData.city_name,resultData.temp,resultData.country_code,resultData.state_code)
-      return this.modelCall
-    })
- };
+export class AppComponent {
+  title = 'Weather Project'
+  
 }
