5e979b29bdf729a6b8b06097902a8f21dd945c64
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-20 11:00:09 -0300

    nova função para os dias futuros

--- src/app/service-api.service.ts
+++ src/app/service-api.service.ts
@@ -7,8 +7,8 @@ import { MODEL_RETURN } from './gateway';
 
 const apiKey = "9fb9b8560938403ab0f9983441b84b9e";
 var apiUrl =`https://api.weatherbit.io/v2.0/current?city=`;
-var finalUrl = ",BR&key=";
-
+var finalUrl = "&lang=pt&BR&key=";
+const historical = "https://api.weatherbit.io/v2.0/forecast/daily?city="
 
 @Injectable({
   providedIn: 'root'
@@ -22,10 +22,21 @@ export class ServiceApiService {
 
 
   modelCall
+  cityHistorical
+
+  getWeatherItems(){
+    return MODEL_RETURN;
+  }
 
   getData(city:string): Observable<ModelResponse[]>{
+    this.cityHistorical = city
     this.modelCall = this.http.get<ModelResponse[]>(apiUrl + city + finalUrl + apiKey);
     return this.http.get<ModelResponse[]>(apiUrl + city + finalUrl + apiKey);
+ }
+
+ dailyForecast(){
+   console.log("funciona")
+    return this.http.get(historical + this.cityHistorical + finalUrl + apiKey + "&days=5")
  }
 
 }
