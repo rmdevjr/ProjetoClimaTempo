5eb4ab56c6a13f4af9c884fbfff68466b12275a2
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-20 11:01:13 -0300

    alteração das funcionalidades no componente busca

--- src/app/buscar-clima/buscar-clima.component.ts
+++ src/app/buscar-clima/buscar-clima.component.ts
@@ -1,47 +1,84 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ServiceApiService } from "../service-api.service";
 import { ModelResponse } from './model.response';
-
+import { ModelFutureResponse } from '../modelFuture.response';
 
 @Component({
   selector: 'app-buscar-clima',
   templateUrl: './buscar-clima.component.html',
   styleUrls: ['./buscar-clima.component.css'],
-  template: `city : {{city}}`
 })
+
 export class BuscarClimaComponent implements OnInit {
 
-  public model:ModelResponse[];
-  estado="bahia";
-  retorno:any;
-  city:any; 
-  modelResult
+  model: ModelResponse[];
+  futureModel: ModelFutureResponse[];
+  estado = "Brasilia";
+  city: any;
+  response: any
+  apiDataReturn:any
+  futureReturn:any
+  futureResponse:any;
 
   constructor(private myservice: ServiceApiService) { }
 
   ngOnInit() {
- };
+    this.myservice.getData(this.estado).subscribe((data) => {
+      this.apiDataReturn = data
+      let resultData = this.apiDataReturn.data[0]
+      this.response = new ModelResponse(
+        resultData.city_name,
+        resultData.temp,
+        resultData.app_temp,
+        resultData.rh,
+        resultData.pres,
+        resultData.wind_spd,
+        resultData.country_code,
+        resultData.state_code,
+        resultData.weather.icon,
+        resultData.weather.description
+      )
+      console.log(this.response)
+      return this.response
+    })
+  };
 
 
- value(event){
-   this.estado = event.target.value
- }
+  value(event) {
+    this.estado = event.target.value
+  }
 
   buscaValor() {
     this.myservice.getData(this.estado).subscribe((data) => {
-      let resultData = Object.create(data).data[0];
-      this.modelResult = new ModelResponse(resultData.city_name,resultData.temp,resultData.country_code,resultData.state_code)
-      this.city = this.modelResult.city_name 
-      return this.modelResult
+      this.apiDataReturn = data
+      let resultData = this.apiDataReturn.data[0]
+      this.response = new ModelResponse(
+        resultData.city_name,
+        resultData.temp,
+        resultData.app_temp,
+        resultData.rh,
+        resultData.pres,
+        resultData.wind_spd,
+        resultData.country_code,
+        resultData.state_code,
+        resultData.weather.icon,
+        resultData.weather.description
+      )
+      console.log(this.response)
+      return this.response
     })
-  }
-
-
-   mostraValor() {
-    
-     this.city = this.retorno;
-     console.log(this.retorno);
-     console.log(this.city);  
+    this.myservice.dailyForecast().subscribe((data)=>{
+      this.futureReturn = data
+      let futureData = this.futureReturn.data[0]
+      this.futureResponse = new ModelFutureResponse(
+        futureData.temp,
+        futureData.app_temp,
+        futureData.rh,
+        futureData.weather.icon,
+        futureData.weather.description
+      )
+      return this.futureResponse
+    })
   }
 
 }
